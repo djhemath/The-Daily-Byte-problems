03620e262d4849be1da3a28555be692bda187d19
DJ Hemath <djhemath@gmail.com>
2024-01-07 06:10:43 +0530

    day19(start-of-cycle): best solution

--- 20.start-of-cycle.js
+++ 20.start-of-cycle.js
@@ -33,6 +33,37 @@ function startOfCycle(head) {
   return null
 }
 
+// T - O(n)
+// S - O(1)
+function startOfCycleBestSolution(head) {
+  // 1. Find the meeting point using Floyd's cycle detection algorithm
+
+  let slow = head;
+  let fast = head;
+
+  while(fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if(slow === fast) break;
+  }
+
+  // 2. If there is no meeting point, then the list has no cycles
+  if(slow !== fast) return null;
+
+  // 3. Keep fast at meeting point and reset slow to head
+  // Move slow and fast pointers one position a time until they both meet
+  slow = head;
+
+  while(slow != fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  // 4. This meeting point will now be the start of cycle
+  return fast;
+}
+
 const test1ListNode1 = new ListNode(1);
 const test1ListNode2 = new ListNode(2);
 const test1ListNode3 = new ListNode(3);
@@ -43,7 +74,8 @@ test1ListNode3.next = null;
 
 const test1List = test1ListNode1;
 
-console.log(startOfCycle(test1List) === null);
+// console.log(startOfCycle(test1List) === null);
+console.log(startOfCycleBestSolution(test1List) === null);
 
 
 const test2ListNode1 = new ListNode(1);
@@ -60,7 +92,8 @@ test2ListNode5.next = test2ListNode2;
 
 const test2List = test2ListNode1;
 
-console.log(startOfCycle(test2List) === test2ListNode2);
+// console.log(startOfCycle(test2List) === test2ListNode2);
+console.log(startOfCycleBestSolution(test2List) === test2ListNode2);
 
 
 const test3ListNode1 = new ListNode(1);
@@ -75,4 +108,5 @@ test3ListNode4.next = test3ListNode4;
 
 const test3List = test3ListNode1;
 
-console.log(startOfCycle(test3List) === test3ListNode4);
+// console.log(startOfCycle(test3List) === test3ListNode4);
+console.log(startOfCycleBestSolution(test3List) === test3ListNode4);
