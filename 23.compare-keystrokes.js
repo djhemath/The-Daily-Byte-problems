7da64d789b38b6f728d5b1d29ff6c6af0b3f112e
DJ Hemath <djhemath@gmail.com>
2024-01-10 07:58:43 +0530

    day23(compare-keystrokes): stack solution

--- 23.compare-keystrokes.js
+++ 23.compare-keystrokes.js
@@ -48,9 +48,68 @@ function compareKeystrokes(s, t) {
   return string1 === string2;
 }
 
-console.log(compareKeystrokes("ABC#", "CD##AB") === true)
-console.log(compareKeystrokes("como#pur#ter", "computer") === true)
-console.log(compareKeystrokes("cof#dim#ng", "code") === false)
-console.log(compareKeystrokes("#", "") === true)
-console.log(compareKeystrokes("#", "#") === true)
-console.log(compareKeystrokes("#", "##") === true)
+// ? NOTE: Both time and space complexities are equal, but removing the last character (pop()) in stack is O(1) whereas in string it's O(n)
+
+// T -> O(m+n)
+// S -> O(m+n)
+function compareKeystrokesWithStack(s, t) {
+  if(typeof s !== 'string') {
+    return false;
+  }
+
+  if(typeof t !== 'string') {
+    return false;
+  }
+
+  let maxLength = s.length > t.length ? s.length: t.length;
+
+  let stack1 = [];
+  let stack2 = [];
+
+  for(let i=0; i<maxLength; i++) {
+    sChar = s[i];
+    tChar = t[i];
+
+    if(sChar) {
+      if(sChar !== '#') {
+        stack1.push(sChar);
+      } else {
+        stack1.pop(sChar);
+      }
+    }
+
+    if(tChar) {
+      if(tChar !== '#') {
+        stack2.push(tChar);
+      } else {
+        stack2.pop(tChar);
+      }
+    }
+  }
+
+  if(stack1.length !== stack2.length) {
+    return false;
+  } else {
+    for(let i=0; i<stack1.length; i++) {
+      if(stack1[i] !== stack2[i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+console.log(compareKeystrokes("ABC#", "CD##AB") === true);
+console.log(compareKeystrokes("como#pur#ter", "computer") === true);
+console.log(compareKeystrokes("cof#dim#ng", "code") === false);
+console.log(compareKeystrokes("#", "") === true);
+console.log(compareKeystrokes("#", "#") === true);
+console.log(compareKeystrokes("#", "##") === true);
+
+console.log(compareKeystrokesWithStack("ABC#", "CD##AB") === true);
+console.log(compareKeystrokesWithStack("como#pur#ter", "computer") === true);
+console.log(compareKeystrokesWithStack("cof#dim#ng", "code") === false);
+console.log(compareKeystrokesWithStack("#", "") === true);
+console.log(compareKeystrokesWithStack("#", "#") === true);
+console.log(compareKeystrokesWithStack("#", "##") === true);
